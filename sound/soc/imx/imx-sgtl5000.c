be72505f194229231f3b718090113c87f19f4b4c
Tapani <tapani@vmail.me>
2012-12-31 17:49:00 +0100

    Enable headphone jack by default on sgtl5000.
    This patch reverts a change introduced by commit eec23c400e5c39ae338b9112031a302230220294
    
    Conflicts:
    	sound/soc/imx/imx-sgtl5000.c
    (cherry picked from commit 39405bfab671b5dbfc3ff7b41b77a0fc954b7824)

--- sound/soc/imx/imx-sgtl5000.c
+++ sound/soc/imx/imx-sgtl5000.c
@@ -247,6 +247,7 @@ static int imx_3stack_sgtl5000_init(struct snd_soc_pcm_runtime *rtd)
 
 	snd_soc_dapm_disable_pin(&codec->dapm, "Line In Jack");
 	snd_soc_dapm_enable_pin(&codec->dapm, "Headphone Jack");
+	snd_soc_dapm_disable_pin(&codec->dapm, "Ext Spk");
 	snd_soc_dapm_sync(&codec->dapm);
 
 	if (hs_jack_gpios[0].gpio != -1) {
