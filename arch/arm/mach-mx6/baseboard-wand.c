fe69f6dbe40a1d99d424d521418152d7d5039f61
John Weber <rjohnweber@gmail.com>
2013-08-03 09:57:04 -0500

    wandboard: Fix checkpatch errors and warnings
    Running scripts/checkpatch.pl -f on baseboard-wand.c results in many
    formatting errors that are not critical but would cause some problems
    if we were to upstream this code.

--- arch/arm/mach-mx6/baseboard-wand.c
+++ arch/arm/mach-mx6/baseboard-wand.c
@@ -12,9 +12,9 @@
 
 
 /****************************************************************************
- *                                                                          
+ *
  * SGTL5000 Audio Codec
- *                                                                          
+ *
  ****************************************************************************/
 
 static struct regulator_consumer_supply wandbase_sgtl5000_consumer_vdda = {
@@ -90,7 +90,7 @@ static struct platform_device wandbase_audio_device = {
 /* ------------------------------------------------------------------------ */
 
 static const struct i2c_board_info wandbase_sgtl5000_i2c_data __initdata = {
-        I2C_BOARD_INFO("sgtl5000", 0x0a)
+	I2C_BOARD_INFO("sgtl5000", 0x0a)
 };
 
 /* ------------------------------------------------------------------------ */
@@ -99,37 +99,40 @@ static char wandbase_sgtl5000_dev_name[8] = "0-000a";
 
 extern struct mxc_audio_platform_data wand_audio_channel_data;
 
-static __init int wandbase_init_sgtl5000(void) {
+static __init int wandbase_init_sgtl5000(void)
+{
 	int i2c_bus = 1; /* TODO: get this from the module. */
 
-        wandbase_sgtl5000_dev_name[0] = '0' + i2c_bus;
+	wandbase_sgtl5000_dev_name[0] = '0' + i2c_bus;
 	wandbase_sgtl5000_consumer_vdda.dev_name = wandbase_sgtl5000_dev_name;
 	wandbase_sgtl5000_consumer_vddio.dev_name = wandbase_sgtl5000_dev_name;
-        
-        wandbase_audio_device.dev.platform_data = &wand_audio_channel_data;
-        platform_device_register(&wandbase_audio_device);
-        
+
+	wandbase_audio_device.dev.platform_data = &wand_audio_channel_data;
+	platform_device_register(&wandbase_audio_device);
+
 	i2c_register_board_info(i2c_bus, &wandbase_sgtl5000_i2c_data, 1);
 	platform_device_register(&wandbase_sgtl5000_vdda_reg_devices);
 	platform_device_register(&wandbase_sgtl5000_vddio_reg_devices);
-        return 0;
+	return 0;
 }
 
 
 /****************************************************************************
- *                                                                          
+ *
  * main-function for wand baseboard
- *                                                                          
- ****************************************************************************/
+ *
+****************************************************************************/
 
-static __init int wandbase_init(void) {
+static __init int wandbase_init(void)
+{
 	return wandbase_init_sgtl5000();
 }
 subsys_initcall(wandbase_init);
 
-static __exit void wandbase_exit(void) {
+static __exit void wandbase_exit(void)
+{
 	/* Actually, this cannot be unloaded. Or loaded as a module..? */
-} 
+}
 module_exit(wandbase_exit);
 
 MODULE_DESCRIPTION("Wand baseboard driver");
